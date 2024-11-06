d29b51eb5903badd1c3d1631484383b60ff70ba8
ihsanye <ihsany98@gmail.com>
2024-11-06 22:28:31 +0300

    register page

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
 
   return (
     <div>
-      ihsan
+
       <RouterConfig />
     </div>
   )
--- src/pages/RegisterPage.tsx
+++ src/pages/RegisterPage.tsx
@@ -1,8 +1,43 @@
 import React from 'react'
+import "../css/RegisterPage.css"
+import TextField from '@mui/material/Textfield'
+import InputAdornment from '@mui/material/InputAdornment'
+import { IoPersonCircleSharp } from 'react-icons/io5'
+import { FaLock } from 'react-icons/fa'
+import { Button } from '@mui/material'
 
 function RegisterPage() {
     return (
-        <div>RegisterPage</div>
+        <div className='register'>
+            <div className='main-div'>
+                <form action="">
+                    <div className='form-div'>
+                        <TextField id='username' placeholder='Username' InputProps={{
+                            startAdornment: (
+                                <InputAdornment position='start'>
+                                    <IoPersonCircleSharp />
+                                </InputAdornment>
+                            )
+                        }}
+                            variant='standard' />
+
+                        <TextField id='password' type='password'
+                            placeholder='Password' InputProps={{
+                                startAdornment: (
+                                    <InputAdornment position='start'>
+                                        <FaLock />
+                                    </InputAdornment>
+                                )
+                            }}
+                            variant='standard' />
+                        <div>
+                            <Button>Kaydol</Button>
+                            <Button>Temizle</Button>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
     )
 }
 
