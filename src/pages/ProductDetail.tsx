1fa700b3cb9bcfb036659e7f4183067d11ba8964
ihsanye <ihsany98@gmail.com>
2024-11-20 15:16:23 +0300

    product detail page styling

--- src/pages/ProductDetail.tsx
+++ src/pages/ProductDetail.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import { Container } from '@mui/material';
+import { Button, Container } from '@mui/material';
 import { useDispatch } from 'react-redux';
 import { setLoading } from '../redux/appSlice';
 import { toast } from 'react-toastify';
@@ -30,7 +30,24 @@ function ProductDetail() {
 
     return (
         <Container maxWidth='lg'>
-            {productId}
+            {product &&
+                <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'center' }}>
+                    <div style={{ margin: '30px 15px' }}>
+                        <img src={product.image} width={200}></img>
+                    </div>
+                    <div>
+                        <h3>{product.title}</h3>
+                        <p>{product.description}</p>
+                        <div style={{ display: 'flex', flexDirection: 'row', alignItems: 'center' }}>
+                            <h2>$ {product.price}</h2>
+                            <span style={{ fontSize: '36px', fontWeight: 'bold', cursor: 'pointer', margin: '0 10px 0 20px' }}>+</span>
+                            <span style={{ fontSize: '18px' }}>0</span>
+                            <span style={{ fontSize: '36px', fontWeight: 'bold', cursor: 'pointer', margin: '0 20px 0 10px' }}>-</span>
+                            <Button color='success' variant='outlined' >Sepete Ekle</Button>
+                        </div>
+                    </div>
+                </div>
+            }
         </Container>
     )
 }
